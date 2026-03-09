6a0ae61e9971999f431480822f222d85778ec81f
qenijsen <115632412+QuintenENijsen@users.noreply.github.com>
2026-03-09 17:10:07 +0100

    Denk nog een keer goed na over de structuur, maybe C++ rewrite.

--- src/ReconstructionAlgorithms/real_truncated_wf.py
+++ src/ReconstructionAlgorithms/real_truncated_wf.py
@@ -1,5 +1,21 @@
+from src.Initialization.pr_init import generate_gaussian_vector, generate_measurement_matrix, generate_measured
+
 
 eps = 1e-05
 MAX_ITER = 10_000
 norm_f0 = 100
 
+def trunc_spectral_init():
+    pass
+
+def compute_trunc_gradient():
+    pass
+
+def gradient_descent():
+    pass
+
+def compute_for_nm():
+    pass
+
+def run_average_sim():
+    pass
